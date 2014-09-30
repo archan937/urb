85d4a1eddb36077e6a811d47ee415411d2a1999f
Paul Engel <pm_engel@icloud.com>
2014-09-30 16:09:46 +0200

    Added urb.js to assets pipeline using the initializer

--- lib/urb/engine.rb
+++ lib/urb/engine.rb
@@ -1,6 +1,10 @@
 module URB
   class Engine < Rails::Engine
 
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w(urb/urb.js)
+    end
+
     initializer "urb.add_middleware" do |app|
       app.middleware.insert 0, URB::Middleware
     end
